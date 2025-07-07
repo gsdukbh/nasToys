0498f8c8e9e889fa674a933405bc38b7982a88e9
JiaWei <gsdukbh@gmail.com>
2025-07-07 15:17:07 +0800

    0.0.1-alpha 🔧 chore(build): 更新版本号为alpha并支持动态替换application.yml中的占位符

--- src/main/java/top/werls/nastoys/NaToysApplication.java
+++ src/main/java/top/werls/nastoys/NaToysApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.info.BuildProperties;
 
-
 /**
  * @author leejiawei
  */
@@ -14,21 +13,26 @@ import org.springframework.boot.info.BuildProperties;
 @Slf4j
 public class NaToysApplication {
 
-
   public static void main(String[] args) {
     var context = SpringApplication.run(NaToysApplication.class, args);
     var env = context.getEnvironment();
     var port = env.getProperty("server.port");
     BuildProperties buildProperties = context.getBean(BuildProperties.class);
-    log.info("""
+    log.info(
+        """
         ==============================================
         = App name : {}
         = App version : {}
         = App build time : {}
         = App run success !!
+        = App web url : http://localhost:{}
         = Swagger doc : http://localhost:{}/swagger-ui.html
         ==============================================
-        """, buildProperties.getName(), buildProperties.getVersion(),buildProperties.getTime(), port);
-
+        """,
+        buildProperties.getName(),
+        buildProperties.getVersion(),
+        buildProperties.getTime(),
+        port,
+        port);
   }
 }
