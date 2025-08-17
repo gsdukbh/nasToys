145ebbfe5318d53c9f2bd14b1d5f0abb5030f46a
JiaWei Lee <gsdukbh@gmail.com>
2025-08-17 22:28:37 +0800

    🚀 feat(deploy): 添加网络模式为host以支持容器间通信 ✨ feat(client): 添加Machine实体

---
+++ deploy/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script installs the necessary dependencies for the project.
+# install jdk
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+sdk install java 21.0.2-graalce
+
+# copy
