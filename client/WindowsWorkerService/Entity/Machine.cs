c8a9780c64a68cc31a109bdeed7c2bf901cad392
JiaWei Lee <gsdukbh@gmail.com>
2025-08-17 22:26:46 +0800

    🚀 feat(deploy): 添加网络模式为host以支持容器间通信 ✨ feat(client): 添加Machine实体

---
+++ client/WindowsWorkerService/Entity/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsWorkerService.Entity;
+
+public class Machine
+{
+    public long? Id { get; set; }
+    public string Name { get; set; }
+    public string Mac { get; set; }
+    public string BroadcastIp { get; set; }
+    public string Ip { get; set; }
+}
